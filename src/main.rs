da30feeccc0ca11d17a8aa8c5d3fa4fb1026c063
housniabdellatif <abdellatife.housni@gmail.com>
2026-09-19 12:13:11 +0100

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
-use std::io::Write;
+use std::io::{Read, Write};
 #[allow(unused_imports)]
 use std::net::TcpListener;
 
 
-const OK_200: &[u8] = "HTTP/1.1 200 OK\r\n\r\n hello".as_bytes();
+const OK_200: &[u8] = "HTTP/1.1 200 OK\r\n\r\n".as_bytes();
+const NOT_FOUND_404: &[u8] = "HTTP/1.1 404 Not Found\r\n\r\n".as_bytes();
+
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -15,12 +17,28 @@ fn main() {
         match stream {
             Ok(mut stream) => {
                 println!("accepted new connection");
-                stream.write(OK_200).unwrap();
-                stream.flush().unwrap();
+                let buf = &mut [0; 1024];
+                stream.read(buf).unwrap();
+                let request_line = str::from_utf8(buf)
+                    .unwrap()
+                    .lines()
+                    .next()
+                    .unwrap();
+
+                let request_target = get_request_target(request_line);
+
+                match request_target {
+                    "/" => {stream.write_all(OK_200).unwrap();}
+                    _ => {stream.write_all(NOT_FOUND_404).unwrap();}
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
+}
+
+fn get_request_target(url: &str) -> &str {
+    url.split(" ").collect::<Vec<&str>>().get(1).unwrap()
 }

9f2c6d34de7676f59590e6fbff3d7a69083315fc
housniabdellatif <abdellatife.housni@gmail.com>
2026-09-20 18:02:48 +0000

    codecrafters submit [skip ci]

--- src/handlers.rs
+++ src/handlers.rs
@@ -37,7 +37,7 @@ pub fn handle_connection(mut stream: TcpStream){
                 close = true;
            }
 
-            if http_request.headers.get("Accept-Encoding").or(Some(&String::from("identity"))).unwrap().to_string() == "gzip" {}) {
+            if http_request.headers.get("Accept-Encoding").or(Some(&String::from("identity"))).unwrap().to_string() == "gzip" {
                 response.headers.insert("Content-Encoding".to_string(), "gzip".to_string());
             }
 
